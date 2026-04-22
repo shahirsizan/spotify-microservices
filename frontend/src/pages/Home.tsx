628dc49262afe5c4e36da2e75bf8641a13668738
shahirsizan <shahir.sizan18@gmail.com>
2026-04-22 23:04:51 +0600

    Home added

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import Layout from "@/components/Layout";
 
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<div className="">
+			<Layout>
+				<div className="mb-4">
+					<h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+				</div>
+
+				<div className="mb-4">
+					<h1 className="my-5 font-bold text-2xl">
+						Today's biggest hits
+					</h1>
+				</div>
+			</Layout>
+		</div>
+	);
 };
 
 export default Home;
