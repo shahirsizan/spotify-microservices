01f291191e5f897a4b41db538a59f9e0df514c9c
shahirsizan <shahir.sizan18@gmail.com>
2026-04-23 00:19:24 +0600

    Navbar added

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,60 @@
+import { useNavigate } from "react-router-dom";
+
+const Navbar = () => {
+	const navigate = useNavigate();
+
+	return (
+		<>
+			<div className="NAVBAR w-full flex justify-between items-center font-semibold">
+				<div className="flex items-center gap-2">
+					<img
+						src="/left_arrow.png"
+						className="w-8 bg-black p-2 rounded-2xl cursor-pointer"
+						onClick={() => navigate(-1)}
+						alt=""
+					/>
+					<img
+						src="/right_arrow.png"
+						className="w-8 bg-black p-2 rounded-2xl cursor-pointer"
+						onClick={() => navigate(+1)}
+						alt=""
+					/>
+				</div>
+
+				<div className="flex items-center gap-4">
+					<p className="px-4 py-1 cursor-pointer bg-white text-black text-[15px] rounded-full hidden md:block">
+						Expore Premium
+					</p>
+
+					<p className="px-4 py-1 cursor-pointer bg-white text-black text-[15px] rounded-full hidden md:block">
+						Install App
+					</p>
+
+					<p className="px-4 py-1 cursor-pointer bg-white text-black text-[15px] rounded-full">
+						Login
+					</p>
+				</div>
+			</div>
+
+			<div className="flex items-center gap-2 mt-4">
+				<p className="bg-white text-black px-4 py-1 rounded-2xl cursor-pointer inline-block">
+					All
+				</p>
+				<p className="bg-white text-black px-4 py-1 rounded-2xl cursor-pointer hidden md:inline-block">
+					Music
+				</p>
+				<p className="bg-white text-black px-4 py-1 rounded-2xl cursor-pointer hidden md:inline-block">
+					Podcasts
+				</p>
+				<p
+					className="bg-white text-black px-4 py-1 rounded-2xl cursor-pointer md:hidden"
+					onClick={() => navigate("/playlist")}
+				>
+					PlayList
+				</p>
+			</div>
+		</>
+	);
+};
+
+export default Navbar;
