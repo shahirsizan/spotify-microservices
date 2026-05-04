79666cef24597712e404195a1399aadb35ee6547
shahirsizan <shahir.sizan18@gmail.com>
2026-05-04 09:12:31 +0600

    Slight modification

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -17,58 +17,56 @@ const Home = () => {
 	 */
 
 	return (
+		// 		{loading ? (
+		// 				<Spinner className="size-20 font-bold text-blue-800" />
+		// 			) : isError ? (
+		// 				<div className="h-screen flex items-center justify-center bg-red-900 text-white text-2xl font-bold">
+		// 					Error!
+		// 				</div>
+		// 			) : (<Layout> code here)
+		// }
 		<div className="">
-			{loading ? (
-				<Spinner className="size-20 font-bold text-blue-800" />
-			) : isError ? (
-				<div className="h-screen flex items-center justify-center bg-red-900 text-white text-2xl font-bold">
-					Error!
+			<Layout>
+				{/* Albums Cards */}
+				<div className="mb-4">
+					<h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+					<div className="flex overflow-auto">
+						{albums?.map((album, idx) => {
+							return (
+								<AlbumCard
+									key={idx}
+									image={album.album_thumb}
+									name={album.album_title}
+									desc={album.album_desc}
+									id={album.album_id}
+								/>
+							);
+						})}
+					</div>
 				</div>
-			) : (
-				<Layout>
-					{/* Albums Cards */}
-					<div className="mb-4">
-						<h1 className="my-5 font-bold text-2xl">
-							Featured Charts
-						</h1>
-						<div className="flex overflow-auto">
-							{albums?.map((album, idx) => {
-								return (
-									<AlbumCard
-										key={idx}
-										image={album.album_thumb}
-										name={album.album_title}
-										desc={album.album_desc}
-										id={album.album_id}
-									/>
-								);
-							})}
-						</div>
-					</div>
 
-					{/* Biggest hits cards */}
-					<div className="mb-4">
-						<h1 className="my-5 font-bold text-2xl">
-							Today's biggest hits
-						</h1>
-						<div className="flex overflow-auto">
-							{songs?.map((song, i) => {
-								return (
-									<SongCard
-										key={i}
-										id={song.id}
-										thumbnail={song.thumbnail}
-										title={song.title}
-										description={song.description}
-										album_id={song.album_id}
-										album_title={song.album_title}
-									/>
-								);
-							})}
-						</div>
+				{/* Biggest hits cards */}
+				<div className="mb-4">
+					<h1 className="my-5 font-bold text-2xl">
+						Today's biggest hits
+					</h1>
+					<div className="flex overflow-auto">
+						{songs?.map((song, i) => {
+							return (
+								<SongCard
+									key={i}
+									id={song.id}
+									thumbnail={song.thumbnail}
+									title={song.title}
+									description={song.description}
+									album_id={song.album_id}
+									album_title={song.album_title}
+								/>
+							);
+						})}
 					</div>
-				</Layout>
-			)}
+				</div>
+			</Layout>
 		</div>
 	);
 };
